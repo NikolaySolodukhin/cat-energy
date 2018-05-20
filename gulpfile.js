d909762252072490fe905261ee686cd841414de7
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2018-05-21 00:39:11 +0300

    Add critical css

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,7 @@ var plugins = require('gulp-load-plugins')({
   ]
 });
 
-var isDevelopment =
-  !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 gulp.task('clean', function () {
   return plugins.del('build');
@@ -34,7 +33,9 @@ gulp.task('clean:css', function () {
 gulp.task('webp', function () {
   return gulp
     .src('source/img/**/*.{png,jpg}')
-    .pipe(plugins.webp({quality: 90}))
+    .pipe(plugins.webp({
+      quality: 90
+    }))
     .pipe(gulp.dest('source/img'));
 });
 
@@ -47,45 +48,42 @@ gulp.task('style', function () {
     .pipe(plugins.if(isDevelopment, plugins.sourcemaps.identityMap()))
     .pipe(plugins.sass())
     .pipe(
-        plugins.postcss([
-          plugins.precss(),
-          plugins.cssMqpacker({
-            sort: true
-          }),
-          plugins.autoprefixer(),
-          plugins.postcssFlexbugsFixes(),
-          plugins.postcssSorting()
-        ]),
-        plugins.if(
-            !isDevelopment,
-            plugins.postcss(
-                plugins.postcssUrl({
-                  url: 'rebase'
-                })
-            )
+      plugins.postcss([
+        plugins.precss(),
+        plugins.cssMqpacker({
+          sort: true
+        }),
+        plugins.autoprefixer(),
+        plugins.postcssFlexbugsFixes(),
+        plugins.postcssSorting()
+      ]),
+      plugins.if(!isDevelopment,
+        plugins.postcss(
+          plugins.postcssUrl({
+            url: 'rebase'
+          })
         )
+      )
     )
     .pipe(plugins.if(isDevelopment, plugins.sourcemaps.write()))
     .pipe(plugins.if(!isDevelopment, plugins.cssnano()))
     .pipe(plugins.if(!isDevelopment, plugins.rev()))
     .pipe(
-        plugins.if(
-            !isDevelopment,
-            plugins.revReplace({
-              manifest: gulp.src('build/manifest/manifest.json')
-            })
-        )
+      plugins.if(!isDevelopment,
+        plugins.revReplace({
+          manifest: gulp.src('build/manifest/manifest.json')
+        })
+      )
     )
     .pipe(gulp.dest('build/css'))
     .pipe(plugins.if(isDevelopment, plugins.plumber.stop()))
     .pipe(
-        plugins.if(
-            !isDevelopment,
-            plugins.rev.manifest('build/manifest/manifest.json', {
-              base: 'build/manifest',
-              merge: true
-            })
-        )
+      plugins.if(!isDevelopment,
+        plugins.rev.manifest('build/manifest/manifest.json', {
+          base: 'build/manifest',
+          merge: true
+        })
+      )
     )
     .pipe(plugins.if(!isDevelopment, gulp.dest('build/manifest')))
     .pipe(plugins.if(isDevelopment, plugins.browserSync.stream()));
@@ -97,43 +95,41 @@ gulp.task('scripts', function () {
     .pipe(plugins.plumber())
     .pipe(plugins.if(isDevelopment, plugins.sourcemaps.init()))
     .pipe(
-        plugins.betterRollup(
-            {
-              plugins: [
-                plugins.rollupPluginNodeResolve({
-                  browser: true
-                }),
-                plugins.rollupPluginCommonjs(),
-                plugins.rollupPluginBabel({
-                  babelrc: false,
-                  exclude: 'node_modules/**',
-                  presets: [
-                    [
-                      'env',
-                      {
-                        modules: false
-                      }
-                    ]
-                  ],
-                  plugins: ['external-helpers']
-                })
-              ]
-            },
-            'iife'
-        )
+      plugins.betterRollup({
+          plugins: [
+            plugins.rollupPluginNodeResolve({
+              browser: true
+            }),
+            plugins.rollupPluginCommonjs(),
+            plugins.rollupPluginBabel({
+              babelrc: false,
+              exclude: 'node_modules/**',
+              presets: [
+                [
+                  'env',
+                  {
+                    modules: false
+                  }
+                ]
+              ],
+              plugins: ['external-helpers']
+            })
+          ]
+        },
+        'iife'
+      )
     )
     .pipe(plugins.if(!isDevelopment, plugins.uglify()))
     .pipe(plugins.if(isDevelopment, plugins.sourcemaps.write('')))
     .pipe(plugins.if(!isDevelopment, plugins.rev()))
     .pipe(gulp.dest('build/js'))
     .pipe(
-        plugins.if(
-            !isDevelopment,
-            plugins.rev.manifest('build/manifest/manifest.json', {
-              base: 'build/manifest',
-              merge: true
-            })
-        )
+      plugins.if(!isDevelopment,
+        plugins.rev.manifest('build/manifest/manifest.json', {
+          base: 'build/manifest',
+          merge: true
+        })
+      )
     )
     .pipe(plugins.if(!isDevelopment, gulp.dest('build/manifest')));
 });
@@ -143,9 +139,9 @@ gulp.task('symbols', function () {
     .src('source/img/icons/*.svg')
     .pipe(plugins.svgmin())
     .pipe(
-        plugins.svgstore({
-          inlineSvg: true
-        })
+      plugins.svgstore({
+        inlineSvg: true
+      })
     )
     .pipe(plugins.rename('sprite.svg'))
     .pipe(plugins.if(!isDevelopment, plugins.rev()))
@@ -158,31 +154,31 @@ gulp.task('images', function () {
   return gulp
     .src('source/img/*.{png,jpg,gif,svg}')
     .pipe(
-        plugins.imagemin([
-          plugins.imagemin.optipng({
-            optimizationLevel: 3
-          }),
-          plugins.imagemin.jpegtran({
-            progressive: true
-          }),
-          plugins.imagemin.svgo()
-        ])
+      plugins.imagemin([
+        plugins.imagemin.optipng({
+          optimizationLevel: 3
+        }),
+        plugins.imagemin.jpegtran({
+          progressive: true
+        }),
+        plugins.imagemin.svgo()
+      ])
     )
     .pipe(gulp.dest('source/img'));
 });
 
 gulp.task('htmlminify', function () {
-  return gulp.src('source/*.html')
+  return gulp.src('build/*.html')
     .pipe(
-        plugins.revReplace({
-          manifest: gulp.src('build/manifest/manifest.json')
-        })
+      plugins.revReplace({
+        manifest: gulp.src('build/manifest/manifest.json')
+      })
     )
     .pipe(plugins.posthtml([plugins.posthtmlInclude()]))
     .pipe(
-        plugins.htmlMinifier2({
-          collapseWhitespace: true
-        })
+      plugins.htmlMinifier2({
+        collapseWhitespace: true
+      })
     )
     .pipe(gulp.dest('build/'));
 });
@@ -190,21 +186,19 @@ gulp.task('htmlminify', function () {
 gulp.task('copy', function () {
   return gulp
     .src(
-        ['source/fonts/*.{woff,woff2}', 'source/img/*.{svg,png,jpg,gif,webp}'],
-        {
-          base: 'source'
-        }
+      ['source/fonts/*.{woff,woff2}', 'source/img/*.{svg,png,jpg,gif,webp}'], {
+        base: 'source'
+      }
     )
     .pipe(plugins.if(!isDevelopment, plugins.rev()))
     .pipe(gulp.dest('build/'))
     .pipe(
-        plugins.if(
-            !isDevelopment,
-            plugins.rev.manifest('build/manifest/manifest.json', {
-              base: 'build/manifest',
-              merge: true
-            })
-        )
+      plugins.if(!isDevelopment,
+        plugins.rev.manifest('build/manifest/manifest.json', {
+          base: 'build/manifest',
+          merge: true
+        })
+      )
     )
     .pipe(plugins.if(!isDevelopment, gulp.dest('build/manifest')));
 });
@@ -240,13 +234,48 @@ gulp.task('server', ['style'], function (fn) {
 
 gulp.task('build', function (fn) {
   plugins.runSequence(
-      'clean',
-      ['symbols'],
-      'copy',
-      ['style', 'scripts'],
-      'htmlminify',
-      fn
+    'clean', ['symbols'],
+    'copy', ['style', 'scripts'],
+    ['critical'],
+    'htmlminify',
+    fn
   );
+});
+
+gulp.task("critical", function () {
+  return gulp
+    .src("source/*.html")
+    .pipe(
+      plugins.critical.generate({
+        base: "./",
+        inline: true,
+        css: "build/css/style.css",
+        minify: true,
+        ignore: ["@font-face", /url\(/],
+        dimensions: [
+          {
+            height: 812,
+            width: 375
+          },
+          {
+            height: 1024,
+            width: 768
+          },
+          {
+            height: 768,
+            width: 1024
+          },
+          {
+            height: 900,
+            width: 1200
+          }
+        ]
+      })
+    )
+    .on("error", function (err) {
+      gutil.log(gutil.colors.red(err.message));
+    })
+    .pipe(gulp.dest("build/"));
 });
 
 gulp.task('demo', function () {
@@ -265,13 +294,13 @@ gulp.task('deploy', function () {
 
 gulp.task('dev', function (fn) {
   plugins.runSequence(
-      'clean',
-      'symbols',
-      'copy',
-      'html:copy',
-      'style',
-      'scripts',
-      'server',
-      fn
+    'clean',
+    'symbols',
+    'copy',
+    'html:copy',
+    'style',
+    'scripts',
+    'server',
+    fn
   );
 });
